aa915615e35dd2f24ed404c9ad8c99303e89e673
Andrii Kachmar <kachmar.andriy07@gmail.com>
2020-10-28 20:28:04 +0200

    Updates

---
+++ precache-manifest.125340c97966f8794ff6c4bec461a9a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ce27f8979844a4b9a49d860ad0a6477",
+    "url": "/robosearch/index.html"
+  },
+  {
+    "revision": "7894035e2434b27c42dc",
+    "url": "/robosearch/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "834d857708478ba55ccd",
+    "url": "/robosearch/static/css/main.71727811.chunk.css"
+  },
+  {
+    "revision": "7894035e2434b27c42dc",
+    "url": "/robosearch/static/js/2.380ec87b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robosearch/static/js/2.380ec87b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "834d857708478ba55ccd",
+    "url": "/robosearch/static/js/main.18f75011.chunk.js"
+  },
+  {
+    "revision": "f0a118fe35fdae25e3d5",
+    "url": "/robosearch/static/js/runtime-main.389cebae.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robosearch/static/media/SEGA.12a3db8b.woff"
+  }
+]);
